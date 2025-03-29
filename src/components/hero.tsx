30caed764c21049f86df74973f8d44779d86a487
heronet <sirat4757@gmail.com>
2025-03-30 01:33:27 +0600

    refactor: update hero component layout and styles; remove unused SVGs and container-text-flip component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,61 @@
-import { ContainerTextFlip } from "./ui/container-text-flip";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "./ui/spotlight";
 
 export default function Hero() {
   return (
-    <div className="p-16 mt-36">
-      <p className="text-4xl font-bold  ml-2">This is Sirat</p>
-      <p className="text-4xl my-4 ml-2">Your firendly neighbourhood</p>
-      <ContainerTextFlip className="" words={["Software Engineer"]} />
+    <div className="relative flex py-50 h-fit w-full overflow-hidden  bg-[#0a0a0a] antialiased md:items-center md:justify-center">
+      <div
+        className={cn(
+          "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
+          "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]"
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
+      <Spotlight />
+
+      <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+        <div className="bg-slate-800 mb-4 mx-auto no-underline group w-fit relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white block">
+          <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+          </span>
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+            <span>Welcome to my website</span>
+          </div>
+          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+        </div>
+
+        <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+          Bringing ideas into reality <br /> with quality code.
+        </h1>
+        <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
+          This is <span className="">Sirat</span>, a Software Engineer
+          specializing in full-stack web development and cross-platform mobile
+          development.
+        </p>
+
+        <button className="relative mx-auto mt-8 flex h-10  overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Let's Explore
+            <svg
+              fill="none"
+              height="22"
+              width="22"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.75 8.75L14.25 12L10.75 15.25"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
