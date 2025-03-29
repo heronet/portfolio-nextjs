4f57f4dcf18d78847fc0f7d0dc9497181b7f01fe
heronet <sirat4757@gmail.com>
2025-03-30 02:19:12 +0600

    add: create overview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import Overview from "@/components/overview";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <Overview />
     </div>
   );
 }
