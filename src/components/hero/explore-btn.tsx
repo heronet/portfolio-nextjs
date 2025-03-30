8d82529d0246d0b45678e9592b5c5d89a2b913c6
Sirat <61063136+heronet@users.noreply.github.com>
2025-03-30 06:44:24 +0600

    Update explore-btn.tsx

--- src/components/hero/explore-btn.tsx
+++ src/components/hero/explore-btn.tsx
@@ -6,7 +6,7 @@ export default function ExploreBtn() {
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-        Let's Explore
+        Let&apos;s Explore
         <svg
           fill="none"
           height="22"
