968682c5774d247da1f461537c72897f47ded5c5
heronet <sirat4757@gmail.com>
2025-04-02 02:10:05 +0600

    feat: open project links in a new tab for improved user experience

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -67,6 +67,7 @@ export default function Project({ project }: ProjectProps) {
         </div>
         <a
           href={project.link}
+          target="_blank"
           className="px-4 w-full  flex items-center justify-center gap-1 my-4 text-center py-2 rounded-md border cursor-pointer border-neutral-300 bg-neutral-100 text-neutral-700 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md"
         >
           Live link <IconExternalLink className="h-4" />
