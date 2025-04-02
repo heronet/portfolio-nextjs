33aef9a15f91f907a36919d7d66d386d0ceb3bf3
heronet <sirat4757@gmail.com>
2025-04-02 06:09:55 +0600

    feat: integrate EmailJS for contact form submission with loading state

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { send } from "@emailjs/browser";
 import { BackgroundGradient } from "../ui/background-gradient";
 import {
   IconBrandGithub,
@@ -10,12 +11,66 @@ import {
   IconBriefcase,
   IconDownload,
 } from "@tabler/icons-react";
+import { useState } from "react";
 
 export default function Contact() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log("Form submitted");
+    if (!formData.email || !formData.message || !formData.name) return;
+
+    console.log(formData);
+
+    if (
+      !process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ||
+      !process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ||
+      !process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+    )
+      return;
+
+    try {
+      setIsLoading(true);
+
+      const data = await send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        {
+          name: formData.name,
+          email: formData.email,
+          message: formData.message,
+        },
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      );
+
+      if (data.status === 200) {
+        alert("I've received your message and will get back to you ASAP!");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+      setIsLoading(false);
+    }
   };
 
   const socialLinks = [
@@ -106,8 +161,10 @@ export default function Contact() {
                       type="text"
                       id="name"
                       name="name"
+                      value={formData.name}
                       className="w-full px-4 py-3 bg-white/10 border border-white/20 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
                       required
+                      onChange={handleChange}
                     />
                   </div>
 
@@ -122,8 +179,10 @@ export default function Contact() {
                       type="email"
                       id="email"
                       name="email"
+                      value={formData.email}
                       className="w-full px-4 py-3 bg-white/10 border border-white/20 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
                       required
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -139,16 +198,20 @@ export default function Contact() {
                     id="message"
                     name="message"
                     rows={4}
+                    value={formData.message}
+                    onChange={handleChange}
                     className="w-full px-4 py-3 bg-white/10 border border-white/20 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition resize-none"
                     required
                   ></textarea>
                 </div>
 
                 <button
+                  disabled={isLoading}
                   type="submit"
-                  className="inline-flex items-center justify-center gap-2 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-xl transition-colors"
+                  className="inline-flex cursor-pointer items-center justify-center gap-2 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-xl transition-colors"
                 >
-                  Send Message
+                  {isLoading ? "Sending..." : "Send Message"}
+
                   <IconSend className="w-4 h-4" />
                 </button>
               </form>
