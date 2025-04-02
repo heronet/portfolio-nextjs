c0abfde9d05e3f39ee5919dc3d186536dcffddd1
heronet <sirat4757@gmail.com>
2025-04-03 02:32:08 +0600

    fix: update button styles and text formatting in ExploreBtn and Intro components for improved UI consistency

--- src/components/hero/explore-btn.tsx
+++ src/components/hero/explore-btn.tsx
@@ -19,10 +19,10 @@ export default function ExploreBtn() {
     >
       <Link
         href="#overview"
-        className="relative w-fit mx-auto mt-8 flex h-10  overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        className="relative w-fit   mx-auto mt-8 flex h-10 transition-all overflow-hidden rounded-3xl hover:rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
       >
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center transition-all rounded-3xl hover:rounded-lg bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
           Let&apos;s Explore
           <svg
             fill="none"
